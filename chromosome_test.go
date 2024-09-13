5072668f1a06005123cb9e429514ce448ef67c6f
João Victor da Silva <jv782063@gmail.com>
2024-09-13 18:16:38 -0300

    test chromosome functions

---
+++ chromosome_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewChromosome(t *testing.T) {
+	n_genes := 10
+	genes := make([]Gene, n_genes)
+	for index := range n_genes {
+		genes[index] = *GenerateGene()
+	}
+	chromosome := newChromosome(genes)
+	assert.NotEmpty(t, chromosome)
+	assert.NotNil(t, chromosome)
+	assert.Equal(t, n_genes, len(chromosome.Genes))
+}
+
+func TestGenerateChromosome(t *testing.T) {
+	randSource := rand.NewSource(time.Now().UnixNano())
+	rnd := rand.New(randSource)
+	for range 500 {
+		n_genes := rnd.Intn(100)
+		t.Run("should return a chromosome with x number of genes", func(t *testing.T) {
+			chromosome := GenerateChromosome(n_genes)
+			assert.NotNil(t, chromosome)
+			assert.NotEmpty(t, chromosome)
+		})
+	}
+}
