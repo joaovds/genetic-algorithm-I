5081e008984e412384068db06dd9d97a0ce9fb82
João Victor da Silva <jv782063@gmail.com>
2024-09-20 12:25:57 -0300

    doing a test to see if the aptitude calculation is correct, placing the closest ones with the highest scores and sorting them by leaving them first

--- pkg/genetic_algorithm.go
+++ pkg/genetic_algorithm.go
@@ -35,15 +35,31 @@ func (g *geneticAlgorithm) Run() {
 	population = initialPopulation
 	targetFound := false
 
+	artificialGenes := make([]*Gene, len(g.Target))
+	for i, value := range g.Target {
+		artificialGenes[i] = NewGene(value)
+	}
+	artificialGenes2 := make([]*Gene, len(g.Target))
+	for i, value := range g.Target {
+		artificialGenes2[i] = NewGene(value)
+	}
+	mockChromosome := NewChromosome(artificialGenes)
+	artificialGenes2[1].Value = rune(107)
+	mockChromosome2 := NewChromosome(artificialGenes2)
+	population.Chromosomes[g.numberOfChromosomes-1] = mockChromosome
+	population.Chromosomes[48] = mockChromosome2
+
 	for generation := range g.MaxGenerations {
 		population.EvaluateFitness(g.Target)
 
 		log.Println("Generation:", generation+1)
-		fmt.Println("Population:")
-		for _, chromosome := range population.Chromosomes {
-			fmt.Println(chromosome.GenesToString(), "=> Fitness:", chromosome.Fitness, "=> Fitness normalized:", chromosome.NormalizedFitness)
+		fmt.Println("Better:", population.Chromosomes[0].GenesToString(), "=> Fitness:", population.Chromosomes[0].Fitness, "=> Fitness normalized:", population.Chromosomes[0].NormalizedFitness)
+		fmt.Println("Middle:", population.Chromosomes[len(population.Chromosomes)/2].GenesToString(), "=> Fitness:", population.Chromosomes[len(population.Chromosomes)/2].Fitness, "=> Fitness normalized:", population.Chromosomes[len(population.Chromosomes)/2].NormalizedFitness)
+		fmt.Println("Worse:", population.Chromosomes[len(population.Chromosomes)-1].GenesToString(), "=> Fitness:", population.Chromosomes[len(population.Chromosomes)-1].Fitness, "=> Fitness normalized:", population.Chromosomes[len(population.Chromosomes)-1].NormalizedFitness)
+
+		for _, a := range population.Chromosomes {
+			fmt.Println(a.GenesToString())
 		}
-		fmt.Println("Population Fitness Total:", population.TotalFitness)
 
 		if targetFound {
 			break
